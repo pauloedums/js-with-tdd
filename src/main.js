da7fa84694f176432006f696638ba5ed940315fb
Paulo Marques <pauloedums@gmail.com>
2021-01-19 12:40:23 -0300

    calc finished

---
+++ src/main.js
@@ -0,0 +1,6 @@
+const sum = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const div = (a, b) =>  (b === 0) ? 'Not possible divide by zero!' : a / b;
+const mult = (a, b) => a * b;
+
+export {sum, sub, div, mult };
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,71 @@
 import { expect } from 'chai';
+import {sum, sub, div, mult } from '../src/main';
 
-describe('Main', () => {
+describe('Calc', () => {
+  let arr;
+  beforeEach(() => {
+    arr = [1, 2, 3];
+  });
+
+  describe('Smoke test', () => {
+
+    it('should exist the method `sum`', () => {
+      expect(sum).to.exist;
+    });
+
+    it('should exist the method `sub`', () => {
+      expect(sub).to.exist;
+    });
+
+    it('should exist the method `div`', () => {
+      expect(div).to.exist;
+    });
+
+    it('should exist the method `mult`', () => {
+      expect(mult).to.exist;
+    });
+  });
+
+  describe('Sum', () => {
+
+    it('should return 4 when `sum(2,2)`', () => {
+      expect(sum(2,2)).to.be.equal(4);
+    });
+
+  });
+
+
+  describe('Sub', () => {
+
+    it('should return 0 when `sum(2,2)`', () => {
+      expect(sub(2,2)).to.be.equal(0);
+    });
+
+    it('should return -4 when `sum(2,2)`', () => {
+      expect(sub(6,10)).to.be.equal(-4);
+    });
+
+  });
+
+
+  describe('Div', () => {
+
+    it('should return 1 when `sum(2,2)`', () => {
+      expect(div(2,2)).to.be.equal(1);
+    });
+
+    it('should return `not possible divide by zero´', () => {
+      expect(div(4,0)).to.be.equal('Not possible divide by zero!');
+    });
+
+  });
+
+  describe('Mult', () => {
+
+    it('should return 4 when `sum(2,2)`', () => {
+      expect(mult(2,2)).to.be.equal(4);
+    });
+
+  });
 
 });
